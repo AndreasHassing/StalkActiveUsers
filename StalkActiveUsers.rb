e8391f4151e58f1f38b4704ab6c7d0f79bddd72b
Andreas Bjørn Hassing Nielsen <abn@Boo.local>
2013-02-20 23:51:49 +0100

    Added the Ruby script
    I recommend you use http://www.youtube.com/all_comments?v=<video_ID>
    instead of watch?v=, as it grants you more comments. I could make sure
    it will always do that.. But right now I just want sleep! I'll do it
    tomorrow.

---
+++ StalkActiveUsers.rb
@@ -0,0 +1,21 @@
+#!/usr/bin/env ruby
+
+require 'open-uri'
+
+youtubeLink = "http://www.youtube.com/all_comments?v=" + ARGV[0]
+if ARGV[0].start_with?("http:")
+	youtubeLink = ARGV[0]
+end
+
+getresult = open(youtubeLink,"UserAgent" => "Ruby-OpenURI").read
+
+title = getresult.scan(/<title>(.*?)<\/title>/)
+
+puts "Fetching most recent/top commenters for: " + title[0][0]
+
+a = getresult.scan(/<a href="\/user\/(.*?)" class="yt-uix-sessionlink yt-user-name "/)
+a.delete_at(0) 	# remove author garbage
+a = a.uniq	# remove duplicates
+
+output = a.transpose.map{|col| col.join(', ')}.join(', ')
+puts output
